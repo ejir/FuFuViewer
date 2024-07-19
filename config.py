ec88c657e9bc4d9e56d6442b5693ea5afec9f80a
ejir <32413408+ejir@users.noreply.github.com>
2024-07-19 16:29:19 +0800

    block_config

---
+++ config.py
@@ -0,0 +1,10 @@
+
+
+#配置类
+class config():
+    def __init__(self) -> None:
+        #小区块宽度
+        self.block_width=200
+        #小区块高度
+        self.block_height=100
+    
--- ultils.py
+++ ultils.py
@@ -14,3 +14,14 @@ def get_image_paths(folder_path):
 def hot_key(e: ft.KeyboardEvent):
     if e.ctrlKey and e.key == "o":
         print("ctrl pressed!")
+
+
+#区块浏览，将gird图分割成多个小块，每个小块都是一个独立的图像
+def block_browse(image_path, block_size):
+    image = ft.imread(image_path)
+    image_height, image_width = image.shape[:2]
+    block_height, block_width = block_size
+    for y in range(0, image_height, block_height):
+        for x in range(0, image_width, block_width):
+            block = image[y:y + block_height, x:x + block_width]
+            yield block, (x, y, block_width, block_height)
